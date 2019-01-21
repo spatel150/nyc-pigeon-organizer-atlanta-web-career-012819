07113a07dbf1c693fa7a789df01809d4d4c72e1f
Samidh Patel <spatel13456@gmail.com>
2019-01-21 02:53:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each do |pigeons, info|
+    info.each do |key, value|
+      value.each do |name| 
+        new_hash[name] ||= {} 
+        new_hash[name][pigeons] ||= []
+        new_hash[name][pigeons] << key.to_s
+      end
+    end
+  end
+  new_hash
 end
