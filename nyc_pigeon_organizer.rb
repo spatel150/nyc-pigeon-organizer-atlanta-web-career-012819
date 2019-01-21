73c67bdb8ee323bfba2c1325d11e7b5cdef95cad
Samidh Patel <spatel13456@gmail.com>
2019-01-21 02:42:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  pigeon_data.each do |pigeons, data|
+    data.each do |key, value|
+      value.each do |name| 
+        new_hash[name] ||= {} 
+        new_hash[name][key] ||= []
+        
 end
